2b1db7c872f9d05a472ab75e074b5221dbe93e2c
sameerq <sameerq@bu.edu>
2016-01-11 00:24:11 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,10 @@
 #Data has been downloaded and unzipped into working directory as a .txt file
 #This stores the data and takes the target dates we want
 mydata <- read.table('household_power_consumption.txt', header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-mydata$Date = as.Date(mydata$Date, format="%d/%m/%Y")
-startDate = as.Date("01/02/2007", format="%d/%m/%Y")
-endDate = as.Date("02/02/2007", format="%d/%m/%Y")
-mydata = mydata[mydata$Date >= startDate & mydata$Date <= endDate, ]
+mydata$Date <- as.Date(mydata$Date, format="%d/%m/%Y")
+startDate <- as.Date("01/02/2007", format="%d/%m/%Y")
+endDate <- as.Date("02/02/2007", format="%d/%m/%Y")
+mydata <- mydata[mydata$Date >= startDate & mydata$Date <= endDate, ]
 
 #Now we make a graph out of the data
 png(filename="plot1.png", width=480, height=480)
