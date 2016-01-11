2670b4049876713f803396fa824834545b422048
sameerq <sameerq@bu.edu>
2016-01-11 00:30:51 -0500

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,22 @@
+#Data has been downloaded and unzipped into working directory as a .txt file
+#This stores the data and takes the target dates we want
+mydata <- read.table('household_power_consumption.txt', header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+mydata$timestamp <- strptime(paste(mydata$Date, mydata$Time),format="%d/%m/%Y %H:%M:%S", tz="UTC")
+startDate <- strptime("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S", tz="UTC")
+endDate <- strptime("02/02/2007 23:59:59", format="%d/%m/%Y %H:%M:%S", tz="UTC")
+mydata <- mydata[mydata$timestamp >= startDate & mydata$timestamp <= endDate, ]
+
+#Now we make a graph out of the data
+png(filename="plot4.png", width=480, height=480)
+par(mfcol=c(2,2))
+#plot 1
+plot(mydata$timestamp, mydata$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+#plot 2
+plot(mydata$timestamp, mydata$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(mydata$timestamp, mydata$Sub_metering_2, col="red")
+lines(mydata$timestamp, mydata$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lwd=par("lwd"), bty="n")
+#plot 3
+plot(mydata$timestamp, mydata$Voltage, type="l", xlab="datetime", ylab="Voltage")
+#plot 4
+plot(mydata$timestamp, mydata$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
